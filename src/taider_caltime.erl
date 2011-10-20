bf8b1d4e57bac164ea827850f3838fa1bc5a2511
Alfonso De Gregorio <adg@crypto.lo.gy>
2011-10-20 16:03:14 +0200

    debugging io:format removed

--- src/taider_caltime.erl
+++ src/taider_caltime.erl
@@ -22,14 +22,11 @@
 
 tai(CT, TID) ->
         Day = taider_caldate:mjd(CT#caltime.date),
-        %%io:format("tai: mjd: ~B~n", [Day]),
 
         S0 = CT#caltime.hour * 60 + CT#caltime.minutes,
         S1 = (S0 - CT#caltime.offset) * 60 + CT#caltime.seconds,
-        %%io:format("tai: s: ~B~n", [S1]),
 
         TAI0 = #tai{ x = Day * 86400 + 4611686014920671114 + S1},
-        %%io:format("tai: pre-leapsec-add: ~B~n", [TAI0#tai.x]),
 
         taider_leapsecs:add(TAI0, CT#caltime.seconds =:= 60, TID).
        
@@ -39,31 +36,23 @@ tai(CT, TID) ->
 utc(TAI, TID) ->
         TAI0 = TAI,
 
-        %%io:format("TAI: ~p~n", [TAI0]),
         {TAI1, Leap} = taider_leapsecs:sub(TAI0, TID),
-        %%io:format("TAI: ~p~n", [TAI1]),
         U    = TAI1#tai.x,
 
         U0   = U + 58486,
         S    = U0 rem 86400,
 
         Sec0 = (S rem 60),
-        %%io:format("Sec0: ~p~n", [Sec0]),
-        %%io:format("Leap: ~p~n", [Leap]),
         Sec1 = if Leap == false -> Sec0; true  -> Sec0 + 1 end,
-        %%io:format("S: ~p~n", [S]),
         S0   = S div 60,
 
         Min0 =  S0 rem 60,
         S1   =  S0 div 60,
 
         U1   = U0 div 86400,
-        %%io:format("U1: ~p~n", [U0]),
         U2   = U1 - 53375995543064,
 
-        %%io:format("U2: ~p~n", [U2]),
         {CD, PWDay, PYDay} = taider_caldate:frommjd(U2),
-        %%io:format("CD: ~p~n", [CD]),
 
         CT = #caltime{
                 date    = CD,
